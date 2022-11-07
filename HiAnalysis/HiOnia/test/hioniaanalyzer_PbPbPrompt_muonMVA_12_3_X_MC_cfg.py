2f6a55f0e5a6f6e9d695964ef6363766c18515e2
Florian <florian.damas@cern.ch>
2022-11-07 17:53:04 +0100

    update selectors

--- HiAnalysis/HiOnia/test/hioniaanalyzer_PbPbPrompt_muonMVA_12_3_X_MC_cfg.py
+++ HiAnalysis/HiOnia/test/hioniaanalyzer_PbPbPrompt_muonMVA_12_3_X_MC_cfg.py
@@ -9,7 +9,7 @@ from Configuration.StandardSequences.Eras import eras
 HLTProcess     = "HLT" # Name of HLT process
 isMC           = True # if input is MONTECARLO: True or if it's DATA: False
 muonSelection  = "All" # Single muon selection: All, Glb(isGlobal), GlbTrk(isGlobal&&isTracker), Trk(isTracker), GlbOrTrk, TwoGlbAmongThree (which requires two isGlobal for a trimuon, and one isGlobal for a dimuon) are available
-applyEventSel  = False # Only apply Event Selection if the required collections are present
+applyEventSel  = True # Only apply Event Selection if the required collections are present
 OnlySoftMuons  = False # Keep only isSoftMuon's (without highPurity, and without isGlobal which should be put in 'muonSelection' parameter) from the beginning of HiSkim. If you want the full SoftMuon selection, set this flag false and add 'isSoftMuon' in lowerPuritySelection. In any case, if applyCuts=True, isSoftMuon is required at HiAnalysis level for muons of selected dimuons.
 applyCuts      = False # At HiAnalysis level, apply kinematic acceptance cuts + identification cuts (isSoftMuon (without highPurity) or isTightMuon, depending on TightGlobalMuon flag) for muons from selected di(tri)muons + hard-coded cuts on the di(tri)muon that you would want to add (but recommended to add everything in LateDimuonSelection, applied at the end of HiSkim)
 SumETvariables = True  # Whether to write out SumET-related variables
@@ -56,7 +56,8 @@ options.outputFile = "Oniatree_MC_miniAOD.root"
 options.secondaryOutputFile = "Jpsi_DataSet.root"
 options.inputFiles =[
   #'/store/himc/HINPbPbAutumn18DR/JPsi_pThat-2_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/AODSIM/mva98_103X_upgrade2018_realistic_HI_v11-v1/120000/06BA15D4-3041-D54E-AB6D-F32A05C95948.root'
-  '/store/himc/HINPbPbSpring21MiniAOD/JPsi_pThat-2_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/MINIAODSIM/mva98_112X_upgrade2018_realistic_HI_v9-v1/100000/016a5b9f-1cd9-442b-a856-c16a8a89e41a.root'
+  #'/store/himc/HINPbPbSpring21MiniAOD/JPsi_pThat-2_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/MINIAODSIM/mva98_112X_upgrade2018_realistic_HI_v9-v1/100000/016a5b9f-1cd9-442b-a856-c16a8a89e41a.root'
+  '/store/user/soohwan/RECO_MC_JPsiEMB_pTHatMin2_CMSSW_12_3_0_220530_v1/HIN-HINPbPbAutumn18GSHIMix-00008_pTHatMin2_EMB_CMSSW_12_3_0/RECO_MC_JPsiEMB_pTHatMin2_CMSSW_12_3_0_220530_v1/220529_160133/0000/step3_RAW2DIGI_L1Reco_RECO_PAT_JPsiEMB_pTHatMin2_10.root'
 ]
 options.maxEvents = 100 # -1 means all events
 
